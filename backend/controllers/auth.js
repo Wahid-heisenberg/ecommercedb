6fddf51d2d8509bfe4b2726ef7e38be9771616a4
wahid_slumani <wahidslimani20002@gmail.com>
2023-12-07 18:32:05 +0100

    implementd login ,register and logout

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,62 +1,95 @@
-import { db } from "../db";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
+import { db } from "../db.js";
+import dotenv from "dotenv";
+dotenv.config();
+
 export const register = (req, res) => {
-  //CHECK EXISTING USER
-  const q = "SELECT * FROM users WHERE email = ? OR username = ?";
+  try {
+    console.log(req.body);
+    if (!req.body.Username || !req.body.User_Password || !req.body.Confirm_Password)
+      return res.status(400).json("Please fill all fields.");
 
-  db.query(q, [req.body.email, req.body.username], (err, data) => {
-    if (err) return res.status(500).json(err);
-    if (data.length) return res.status(409).json("User already exists!");
+    if (req.body.User_Password.length < 6 || req.body.Confirm_Password.length < 6)
+      return res
+        .status(400)
+        .json("Password should contain more than 6 caracters.");
 
-    //Hash the password and create a user
-    const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync(req.body.password, salt);
+    if (req.body.User_Password !== req.body.Confirm_Password)
+      return res.status(400).json("Passwords don't match.");
+    //Check if user exists
+    const q = "SELECT * FROM users WHERE  Username = ?";
 
-    const q = "INSERT INTO users(`username`,`email`,`password`) VALUES (?)";
-    const values = [req.body.username, req.body.email, hash];
-
-    db.query(q, [values], (err, data) => {
+    db.query(q, [req.body.Username], (err, data) => {
       if (err) return res.status(500).json(err);
-      return res.status(200).json("User has been created.");
+      if (data.length) return res.status(409).json("User already exists!");
+
+      //Hash the password and create a user
+
+      const salt = bcrypt.genSaltSync(10);
+      const hash = bcrypt.hashSync(req.body.User_Password, salt);
+
+      const q = "INSERT INTO Users(`Username`,`User_Password`) VALUES (?)";
+      const values = [req.body.Username, hash];
+
+      db.query(q, [values], (err, data) => {
+        if (err) return res.status(500).json("Database error :<br/>" + err);
+        return res.status(200).json("User has been created.");
+      });
     });
-  });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const login = (req, res) => {
-  //CHECK USER
+  try {
 
-  const q = "SELECT * FROM users WHERE username = ?";
+    if (!req.body.Username || !req.body.User_Password)
+      return res.status(400).json("Please fill all fields.");
 
-  db.query(q, [req.body.username], (err, data) => {
-    if (err) return res.status(500).json(err);
-    if (data.length === 0) return res.status(404).json("User not found!");
+    if (req.body.User_Password.length < 6)
+      return res
+        .status(400)
+        .json("Password should contain more than 6 caracters.");
 
-    //Check password
-    const isPasswordCorrect = bcrypt.compareSync(
-      req.body.password,
-      data[0].password
-    );
+    const q = "SELECT * FROM users WHERE Username = ? ";
 
-    if (!isPasswordCorrect)
-      return res.status(400).json("Wrong username or password!");
+    db.query(q, [req.body.Username], (err, data) => {
+      if (err) return res.status(500).json(err);
+      if (data.length === 0) return res.status(404).json("User not found!");
 
-    const token = jwt.sign({ id: data[0].id }, "jwtkey");
-    const { password, ...other } = data[0];
+      //Check password
+      const isPasswordCorrect = bcrypt.compareSync(
+        req.body.User_Password,
+        data[0].User_Password
+      );
 
-    res
-      .cookie("access_token", token, {
-        httpOnly: true,
-      })
-      .status(200)
-      .json(other);
-  });
+      if (!isPasswordCorrect)
+        return res.status(400).json("Wrong username or password!");
+
+      const token = jwt.sign({ id: data[0].id }, process.env.JWT_SECRET);
+      const { User_Password, ...other } = data[0];
+
+      res
+        .cookie("access_token", token, {
+          httpOnly: true,
+        })
+        .status(200)
+        .json(other);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const logout = (req, res) => {
-  res.clearCookie("access_token",{
-    sameSite:"none",
-    secure:true
-  }).status(200).json("User has been logged out.")
+  res
+    .clearCookie("access_token", {
+      sameSite: "none",
+      secure: true,
+    })
+    .status(200)
+    .json("User has been logged out.");
 };
--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,16 @@ import dotenv from "dotenv";
 import cors from "cors";
 import { db } from "./db.js";
 
-const app = express();
+import authRoutes from "./routes/auth.js";
 dotenv.config();
+
+const app = express();
 app.use(cors());
 app.use(express.json());
+
+
+app.use("/api/auth", authRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 
