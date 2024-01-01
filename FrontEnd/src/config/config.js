96e80feb837c5476e4952d0a14b1c9c9e1d68bbf
SamyIslem <samyislemoy001@gmail.com>
2024-01-01 14:44:57 +0100

    a

---
+++ FrontEnd/src/config/config.js
@@ -0,0 +1,10 @@
+export const products = [
+    {
+      Id_product: 1,
+      Name: "",
+      description: "",
+      Price: 0,
+      Id_category: 1,
+      Id_Sub_Category: 1,
+    },
+  ];
--- FrontEnd/src/pages/Home/Home.jsx
+++ FrontEnd/src/pages/Home/Home.jsx
@@ -1,21 +1,18 @@
-import React from 'react'
-import { Title } from '@/ui'
-
+import React from "react";
+import { Title } from "@/ui";
 
 const Home = () => {
   return (
     <>
-      <main className='relative'>
-        <div className='c-container flex flex-col gap-2'>
-        <Title>Today's</Title>
-        <h1 className='font-bold text-xl' >New products</h1>
-        
-        
-         </div>
-
+      <main className="relative">
+        <div className="c-container flex flex-col gap-2">
+          <Title>Today's</Title>
+          <h1 className="font-bold text-xl">New products</h1>
+          
+        </div>
       </main>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
